67e491bea8e2886fc5586c5cfdd81892cbcfb006
Ursa Kese <ursa.kese@gmail.com>
2023-04-12 18:31:43 +0200

    Add app and index

--- siohca/src/App.js
+++ siohca/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// rafce -> React Arrow Function Component with Export
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {Footer, Home, Faq, Team, Pub, Contacts, EuReCa3, Posts} from './containers';
+import { ReactComponent as Logo } from  './assets/svg/brand/logo-small.svg';
+import {Navbar} from './components';
+
+
+// Reload window to top on refresh
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 
 function App() {
+
+  // Loading screen
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1800);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+       {loading ? (
+        <div className="loader-container" >
+      	  <div className="spinner"> <Logo/> </div>
+        </div>
+      ) : (
+        <div className="main-content">
+            
+          <BrowserRouter>
+          <Navbar />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/team' element={<Team/>} />
+              <Route path='/posts' element={<Posts/>} />
+              <Route path='/publications&press' element={<Pub/>} />
+              <Route path='/eureca3' element={<EuReCa3/>} />
+              <Route path='/contacts' element={<Contacts/>} />
+          </Routes>
+          </BrowserRouter>
+      
+    </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default App
+
