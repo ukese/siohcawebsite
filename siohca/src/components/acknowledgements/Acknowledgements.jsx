951753bb31ecb7554d8e9c5cc4cb2cf0b02c97b2
Ursa Kese <ursa.kese@gmail.com>
2023-06-07 13:29:00 +0200

    Hide scroll overflow

--- siohca/src/components/acknowledgements/Acknowledgements.jsx
+++ siohca/src/components/acknowledgements/Acknowledgements.jsx
@@ -6,27 +6,25 @@ import { AcknowledgementsItems } from "./AcknowledgementsItems";
 
 const Acknowledgements = () => {
   return (
-    <Fade bottom>
-      <div className="acknowledgements">
-        <div className="acknowledgements-title">
-          <h2>We would like to thank</h2>
-        </div>
-        <div className="acknowledgements-content">
-          {AcknowledgementsItems.map((item, index) => {
-            return (
-              <div className="acknowledgements-content-box margin-bottom-1">
-                <Link to={item.link}>
-                  <p>
-                    <h3 className="margin-bottom-1">{item.name}</h3>
-                    {item.text}
-                  </p>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+    <div className="acknowledgements">
+      <div className="acknowledgements-title">
+        <h2>We would like to thank</h2>
       </div>
-    </Fade>
+      <div className="acknowledgements-content">
+        {AcknowledgementsItems.map((item, index) => {
+          return (
+            <div className="acknowledgements-content-box margin-bottom-1">
+              <Link to={item.link}>
+                <p>
+                  <h3 className="margin-bottom-1">{item.name}</h3>
+                  {item.text}
+                </p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
